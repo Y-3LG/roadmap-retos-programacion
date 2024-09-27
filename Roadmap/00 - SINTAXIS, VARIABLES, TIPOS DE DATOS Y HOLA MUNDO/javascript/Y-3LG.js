c5cb4465443c838c0b2909c178d260e9c5b8d70d
Yanitho's <77212067+Y-3LG@users.noreply.github.com>
2024-09-27 01:04:33 -0400

    #00 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Y-3LG.js
@@ -0,0 +1,40 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//One line comment
+
+/* 
+    Multi-line 
+    
+            comment
+                    */
+
+//////////////////////////////
+
+var a_var = 1;
+
+let anotherVar = 2;
+
+const a_const = 3;
+
+//////////////////////////////
+
+let $boolean = true;
+
+let $null = null;
+
+let $undefined = undefined;
+
+let $number = 54;
+
+let $BigInt = 4568213131464n;
+
+let $string = 'Thanks Js';
+
+let $symbol = Symbol();
+
+let $object = Object.create();
+
+//////////////////////////////
+
+console.log('Hello &', $string)
+
